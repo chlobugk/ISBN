2b21bc2b3a8b1ffdf3e967670d52af796a3ff245
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-02-02 11:20:22 -0500

    passes with no letters except x

--- isbn_exercise.rb
+++ isbn_exercise.rb
@@ -1,12 +1,45 @@
-def valid_function(num)
-	# total = 0
+def valid_digits(num)
+# passing in a string, return a boolean
 
 	if num.length == 10
-		true
-	elsif num.length == 13
 		true
 	else 
 		false
 	end
 end 
 
+def remove_spaces(isbn_num)
+
+	# isbn_num.delete(' ' '-')
+
+	isbn_num.gsub(/[ -]/, '')
+
+end
+
+def no_letters(number)
+
+	if number.match(/[a-z A-Z]/)
+		false
+
+
+	else
+		true
+
+	end
+end
+
+def yes_to_x(numb)
+
+	if numb[-1].match(/[0-9 x X]/)
+		true
+	else 
+		false
+	end
+end
+
+
+
+
+
+
+	
--- test_isbn_exercise.rb
+++ test_isbn_exercise.rb
@@ -1,8 +1,56 @@
 require 'minitest/autorun'
 require_relative 'isbn_exercise.rb'
 
-class TestIsbnFunction < MiniTest::Test 
-	def test_number_returns_true
-		assert_equal(true, valid_function('1234567890'))
+class TestIsbnLength < MiniTest::Test 
+	def test_10_returns_true
+		num = '1234567890'
+		assert_equal(true, valid_digits(num))
 	end
+
+	def test_9_returns_false
+		num = '123456789'
+		assert_equal(false, valid_digits(num))
+	end
+
+	def test_length_true_not_isbn
+		num = '1234X67890'
+		assert_equal(true, valid_digits(num))
+	end
+ 
+	def test_10_with_spaces_returns
+		num = '123 456 7890'
+		assert_equal('1234567890', remove_spaces(num))
+	end
+
+	def test_10_with_hyphens_returns
+		num = '123-45--67-890'
+		assert_equal('1234567890', remove_spaces(num))
+	end
+
+	def test_letter_returns_false
+		num = '12345b7890'
+		assert_equal(false, no_letters(num))
+	end
+
+	def test_no_letter_returns_true
+		num = '1234567890'
+		assert_equal(true, no_letters(num))
+	end
+
+	def test_last_x_returns_true
+		num = '123456789x'
+		assert_equal(true, yes_to_x(num))
+	end
+
+	def test_numb_returns_true
+		num = '1234567890'
+		assert_equal(true, yes_to_x(num))
+	end
+
+	def test_returns_false
+		num = '123456789s'
+		assert_equal(false, yes_to_x(num))
+	end
+
+
 end
