63161e3e9d033cb149ded79f4075846ba6a9f62d
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-02-09 10:19:59 -0500

    FINISHED ISBN yaaayyy

--- isbn_exercise.rb
+++ isbn_exercise.rb
@@ -11,11 +11,11 @@ end
 
 
 
-def remove_spaces(isbn_num)
+def remove_spaces_dashes(isbn_num)
 
-	isbn_num.delete(' ' '-')
+	# isbn_num.delete(' ' '-')
 
-	# isbn_num.gsub(/[ -]/, '')
+	isbn_num.gsub(/[ -]/, '')
 end
 
 
@@ -53,26 +53,26 @@ end
 
 
 
-def check_digit(check)
+def check_digit_10(num)
 	valid = false
 	total = 0
 	index_num = 0 #1st number is in 0 position
 	index_pos = 1 #each digit: 1st, 2nd, etc
 	
 
-	counter = check.chop
+	counter = num.chop
 	counter.length.times do 
 
-		total = total + check[index_num].to_i * index_pos 
+		total = total + num[index_num].to_i * index_pos 
 		index_num = index_num + 1
 		index_pos = index_num + 1
 	end
 
 	digit = total % 11
 
-	if digit == 10 && check[-1].match(/[xX]/) 
+	if digit == 10 && num[-1].match(/[xX]/) 
 		valid = true
-	elsif digit == check[-1].to_i 
+	elsif digit == num[-1].to_i 
 		valid = true	
 	
 	end
@@ -110,6 +110,28 @@ def check_digit_13(num)
 		valid
 end
 
+
+def final_isbn(number)
+	valid = false
+	final = remove_spaces_dashes(number)
+
+		if final.length == 10
+			no_letters(final)
+			no_symbols(final)
+			yes_to_x(final)
+			if check_digit_10(final) == true
+				valid = true
+			end
+
+		else final.length == 13
+			no_letters(final)
+			no_symbols(final)
+			if check_digit_13(final) == true
+				valid = true
+			end
+		end
+	valid
+end
 
 
 
--- test_isbn_exercise.rb
+++ test_isbn_exercise.rb
@@ -19,12 +19,12 @@ class TestIsbnLength < MiniTest::Test
  
 	def test_10_with_spaces_returns
 		num = '123 456 7890'
-		assert_equal('1234567890', remove_spaces(num))
+		assert_equal('1234567890', remove_spaces_dashes(num))
 	end
 
 	def test_10_with_hyphens_returns
 		num = '123-45--67-890'
-		assert_equal('1234567890', remove_spaces(num))
+		assert_equal('1234567890', remove_spaces_dashes(num))
 	end
 
 	def test_letter_returns_false
@@ -63,18 +63,18 @@ class TestIsbnLength < MiniTest::Test
 	end
 
 	def test_check_digit_returns_true
-		check = '0471958697'
-		assert_equal(true, check_digit(check))
+		num = '0471958697'
+		assert_equal(true, check_digit_10(num))
 	end
 
 	def test_check_also_returns_true
-		check = '0-321-14653 0'
-		assert_equal(true, check_digit(check))
+		num = '0-321-14653 0'
+		assert_equal(true, check_digit_10(num))
 	end
 
 	def test_check_returns_false
-		check = '7421394 762'
-		assert_equal(false, check_digit(check))
+		num = '7421394 762'
+		assert_equal(false, check_digit_10(num))
 	end
 end
 
@@ -87,12 +87,12 @@ class TestIsbn13 < MiniTest::Test
 
 	def test_13_with_spaces_returns
 		num = '123 456 789 0123'
-		assert_equal('1234567890123', remove_spaces(num))
+		assert_equal('1234567890123', remove_spaces_dashes(num))
 	end
 
 	def test_13_with_hyphens_returns
 		num = '098 765-678-4 324'
-		assert_equal('0987656784324', remove_spaces(num))
+		assert_equal('0987656784324', remove_spaces_dashes(num))
 	end
 
 	def test_no_letters
@@ -116,14 +116,49 @@ class TestIsbn13 < MiniTest::Test
 	end
 
 	def test_check_13_false
-		num = '97801314 95057'
+		num = '9780131495057'
 		assert_equal(false, check_digit_13(num))
 	end
 
 end
 
-# class TestIsbnValid < MiniTest::Test 
-# 	def 
+	#BELOW IS THE FINAL FUNCTION
+
+class TestIsbnValid < MiniTest::Test 
+	def test_10_passes_true
+		num = '0-321-14653 0'
+		assert_equal(true, final_isbn(num))
+	end
+
+	def test_13_passes_true
+		num = '97801-31495050'
+		assert_equal(true, final_isbn(num))
+	end
+
+	def test_returns_false
+		num = '9780131495055'
+		assert_equal(false, final_isbn(num))
+	end
+
+	def test_symbols_false
+		num = '047@958697'
+		assert_equal(false, final_isbn(num))
+	end
+
+	def test_x_true
+		num = '877195869x'
+		assert_equal(true, final_isbn(num))
+	end
+
+	def test_capital_X_true
+		num = '877195869X'
+		assert_equal(true, final_isbn(num))
+	end
+
+	def test_letter_false
+		num = '877x958691'
+		assert_equal(false, final_isbn(num))
+	end
 
 
-# end
+end
